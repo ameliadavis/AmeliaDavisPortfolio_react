c260d55a187afca2f2923af37df69c499d535fee
Amelia Davis <ameliaaltman22@gmail.com>
2020-05-09 21:34:54 -0600

    added style to header

--- react-portfolio/src/components/header.js
+++ react-portfolio/src/components/header.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./header.css";
+import {HeaderStyle} from "../styles";
 
 const header = props => {
     return(
         <div>
-             {/* <img className = "logo" src = {`${process.env.PUBLIC_URL}/assets/AD_Logo.png`} /> */}
-             <h1>Amelia Davis</h1>
-            <video id="background-video" loop autoPlay>
-                <source src={`${process.env.PUBLIC_URL}/assets/HeaderVideo_Red.mp4`} type="video/mp4" width = "100%"/>
-                <source src={`${process.env.PUBLIC_URL}/assets/HeaderVideo_Red.mp4`} type="video/ogg" />
-                Your browser does not support the video tag.
-            </video>
-           
+            <HeaderStyle>
+                <img className = "logo" src = {`${process.env.PUBLIC_URL}/assets/AD_Logo.png`} />
+                <video id="background-video" loop autoPlay>
+                    <source src={`${process.env.PUBLIC_URL}/assets/PeonyBackground.mp4`} type="video/mp4" width = "100%"/>
+                    <source src={`${process.env.PUBLIC_URL}/assets/PeonyBackground.mp4`} type="video/ogg" />
+                    Your browser does not support the video tag.
+                </video>
+            </HeaderStyle>                
         </div>
     )}
 
--- react-portfolio/src/styles/index.js
+++ react-portfolio/src/styles/index.js
@@ -1,16 +1,30 @@
 import styled from "styled-components";
 
-const aqua = "#1FCDC9";
-const orange = "#FF9E18";
+// const aqua = "#1FCDC9";
+// const orange = "#FF9E18";
 
-export const AmeliaStyle = styled.article `
+export const HeaderStyle = styled.div `
+.section {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex; 
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+}
 video {
-    position: absolute;
+    position: absolute ;
     top: 0;
     left: 0;
-    width: 100;
+    width: 100%
+    z-index: 1;
 }
-
-
-
+.logo{
+    bottom: 60%
+    width: 25%
+    padding: 20px;
+    left-margin: 500px;
+    z-index: 3;
+}
 `
