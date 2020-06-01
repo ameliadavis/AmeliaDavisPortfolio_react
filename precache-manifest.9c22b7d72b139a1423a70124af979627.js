32468a6bf8ac9e65833b4202b6e34cf417bf5f90
Amelia Davis <ameliaaltman22@gmail.com>
2020-05-31 20:56:46 -0600

    Updates

---
+++ precache-manifest.9c22b7d72b139a1423a70124af979627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d416ade24398059a8311432de98ffdf0",
+    "url": "/AmeliaDavisPortfolio_react/index.html"
+  },
+  {
+    "revision": "af4673471f6c6053b055",
+    "url": "/AmeliaDavisPortfolio_react/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "af4673471f6c6053b055",
+    "url": "/AmeliaDavisPortfolio_react/static/js/2.6eda88ca.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AmeliaDavisPortfolio_react/static/js/2.6eda88ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c97e1fc93ebeaaec80a",
+    "url": "/AmeliaDavisPortfolio_react/static/js/main.56c09d3f.chunk.js"
+  },
+  {
+    "revision": "10079bbb97ceb0df9885",
+    "url": "/AmeliaDavisPortfolio_react/static/js/runtime-main.4caf5a50.js"
+  }
+]);
